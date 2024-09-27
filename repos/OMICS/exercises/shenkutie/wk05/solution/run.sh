3194eeaeaeac12efda1ca1cdae3e5c026671843d
shenkutie <ts3576@drexel.edu>
2024-09-26 21:23:16 -0400

    found barcodded data

--- repos/OMICS/exercises/shenkutie/wk05/solution/run.sh
+++ repos/OMICS/exercises/shenkutie/wk05/solution/run.sh
@@ -1,5 +1,11 @@
-  #ln -s ../../../../content/wk05/data/multiplexed.fq
-  #ln -s ../../../../content/wk05/data/sample_sheet.csv 
-  wc -l sample_sheet.csv 
-  cat sample_sheet.csv
-  seqkit stats multiplexed.fq  | tee muxed.stats.tsv 
+#ln -s ../../../../content/wk05/data/multiplexed.fq
+#ln -s ../../../../content/wk05/data/sample_sheet.csv 
+wc -l sample_sheet.csv 
+cat sample_sheet.csv
+seqkit stats multiplexed.fq  | tee muxed.stats.tsv 
+#sample_name,forward,reverse
+#SRR23803536,ACCT,CCAG
+#SRR23803537,ACCT,TCAG
+#SRR23803538,CTGC,CCAG
+#SRR23803539,CTGC,TCAG
+seqkit grep -srip ^ACCT multiplexed.fq |seqkit grep -srip CCAG$ -o SRR23803536.fastq
